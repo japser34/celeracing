9faaf771354a67108558f2f135b73588153b83c8
japser34 <100087296+japser34@users.noreply.github.com>
2025-04-07 21:40:17 +0200

    Added team images

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
 function slugify(title: string): string {
   return title.toLowerCase().replace(/\s+/g, "-");
@@ -12,14 +14,20 @@ export default function TeamCard({
   name: string;
   team: string;
 }) {
+  const [imageSrc, setImageSrc] = useState(`/team/${slugify(name)}.png`);
+
+  const handleError = () => {
+    setImageSrc("/team/empty.png"); // Fallback image
+  };
   return (
     <div className={styles.teamCard}>
       <Image
-        src={`/team/${slugify(name)}.png`}
+        src={imageSrc}
         alt={name}
         width={150}
         height={150}
         className={styles.teamImage}
+        onError={handleError} // Handle error event
       />
       <h3>{name}</h3>
       <p>{team}</p>
