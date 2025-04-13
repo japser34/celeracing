539fac5026ee68bce9dbb4745092bf65d0ff4674
japser34 <100087296+japser34@users.noreply.github.com>
2025-04-13 21:27:00 +0200

    improved the 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,25 @@
+import Image from "next/image";
+
 export default function NotFound() {
-  return <h1>404 - Page Not Found (:</h1>;
+  return (
+    <div>
+      {/* Fixed background image */}
+      <div className="start-background">
+        <Image
+          src="/not-found.png"
+          alt="Background"
+          fill
+          className="object-cover"
+        />
+      </div>
+
+      <section className="start-text">
+        <h1>
+          <div style={{ fontSize: "clamp(3rem, 20vh, 30rem)" }}>:(</div>
+          <div>We are not able to find that page,</div>
+          <div>or we lost it.</div>
+        </h1>
+      </section>
+    </div>
+  );
 }
